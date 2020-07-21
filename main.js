77b43608fbea0d41789a7bb9f603212287197306
ReginaF2012 <skritschy@gmail.com>
2020-07-21 12:41:08 -0500

    Done.

--- main.js
+++ main.js
@@ -3,6 +3,39 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const main = document.querySelector('body')
+const errorModal = document.getElementById('modal')
+
+function addLikes(e){
+  if (e.target.nodeName = "LI" ){
+    let heart = e.target.firstElementChild
+    mimicServerCall('url')
+    .then(function(){
+      if (heart.innerText == EMPTY_HEART){
+        heart.innerText = FULL_HEART
+        heart.className = "activated-heart"
+        // heart.style.color = 'red'
+      } else {
+        heart.innerText = EMPTY_HEART
+        heart.className = ""
+        // heart.style.color = e.target.style.color
+      }
+    })
+    .catch(function(errors){
+      errorModal.className = ""
+      errorModal.querySelector('p').innerText = errors
+      setTimeout(function hideModal(modal) {
+        modal = errorModal
+        modal.className = "hidden"
+      }, 5000)
+    })
+  }
+
+};
+// One event listener to rule them all?
+main.addEventListener('click', addLikes)
+
+
 
 
 
